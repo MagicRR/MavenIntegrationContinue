51f849a9cd642b2e5df7fe79da6b11d09f137322
MagicRR <r.richard.94@orange.fr>
2017-12-04 11:32:15 +0100

    Premier Commit, initilisation projet.

---
+++ MavenIntegrationContinue/src/main/java/MavenIntegrationContinue/MavenIntegrationContinue/App.java
@@ -0,0 +1,13 @@
+package MavenIntegrationContinue.MavenIntegrationContinue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
